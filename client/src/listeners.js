5d10fdda630ce49c54ebac72d2960146a7c5c460
David <david.weppler@outlook.com>
2019-09-11 16:01:42 +0200

    issue with double declared listener

--- client/src/listeners.js
+++ client/src/listeners.js
@@ -65,18 +65,6 @@ socket.on('setOnTurn', (onTurn) => {
   this._onTurn = onTurn;
 })
 
-const addTurnListeners = () => {
-  ['button0', 'button1', 'button2',
-   'button3', 'button4', 'button5',
-   'button6', 'button7', 'button8'].forEach((id) => {
-    const field = document.getElementById(id);
-    field.addEventListener('click', () => {
-      if(this._onTurn == true && document.getElementById(id).innerHTML == '') {
-        socket.emit('turn', document.getElementById(id).value);
-      }
-    });
-  });
-};
 
 document
   .querySelector('#chat-form')
