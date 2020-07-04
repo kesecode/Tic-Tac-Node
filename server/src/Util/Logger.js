4460d1c1811ea02c6d0315284343b92c8420f162
dweppler <contact@david-weppler.de>
2020-07-04 14:02:11 +0200

    commented out app use forceLiveDomain

--- server/src/Util/Logger.js
+++ server/src/Util/Logger.js
@@ -1,24 +1,47 @@
 class Logger {
-    userConnection(socket, isConnected, usersOnline) {
-        if(isConnected) {
-            console.log('\x1b[42m', '******* SERVER:        USER CONNECTED       ******* SOCKET-ID:  [', socket.id, ']', '\x1b[0m', 'Online Users: ', usersOnline);
-        } else {
-            console.log('\x1b[43m', '******* SERVER:        USER DISCONNECTED    ******* SOCKET-ID:  [', socket.id, ']', '\x1b[0m','Online Users: ', usersOnline);
-        }
-    }
-  
-    matchInitialized(p0, p1) {
-        console.log('\x1b[34m', '******* CONTROLLER:    MATCH INITIALIZED    ******* Player 0:  [', p0.name, '] -- Player 1:  [', p1.name, ']', '\x1b[0m');
-    }
-  
-    serverError(err) {
-        console.error('\x1b[41m', '******* SERVER - ERROR: ', err, '\x1b[0m');
-    }
-  
-    serverStarted(port) {
-        console.log('\x1b[35m', '******* SERVER: listening to requests on port ', port, ' *******', '\x1b[0m');
-    }
+	userConnection(socket, isConnected, usersOnline) {
+		if (isConnected) {
+			console.log(
+				'\x1b[42m',
+				'******* SERVER:        USER CONNECTED       ******* SOCKET-ID:  [',
+				socket.id,
+				']',
+				'\x1b[0m',
+				'Online Users: ',
+				usersOnline
+			);
+		} else {
+			console.log(
+				'\x1b[43m',
+				'******* SERVER:        USER DISCONNECTED    ******* SOCKET-ID:  [',
+				socket.id,
+				']',
+				'\x1b[0m',
+				'Online Users: ',
+				usersOnline
+			);
+		}
+	}
+
+	matchInitialized(p0, p1) {
+		console.log(
+			'\x1b[34m',
+			'******* CONTROLLER:    MATCH INITIALIZED    ******* Player 0:  [',
+			p0.name,
+			'] -- Player 1:  [',
+			p1.name,
+			']',
+			'\x1b[0m'
+		);
+	}
+
+	serverError(err) {
+		console.error('\x1b[41m', '******* SERVER - ERROR: ', err, '\x1b[0m');
+	}
+
+	serverStarted(port) {
+		console.log('\x1b[35m', '******* SERVER: listening to requests on port ', port, ' *******', '\x1b[0m');
+	}
 }
 
 module.exports = Logger;
-  
